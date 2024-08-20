b13ffeee8247a6c10a8a21b13eee25ad812ea6b2
Abdur Rahman <abdur.rahman.cui@gmsil.com>
2024-08-20 20:09:04 +0500

    task Complete

---
+++ api-integration/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import DummyApi from './components/DummyApi';
+
+function App() {
+  return (
+    <>
+      <DummyApi />
+    </>
+  );
+}
+
+export default App;
